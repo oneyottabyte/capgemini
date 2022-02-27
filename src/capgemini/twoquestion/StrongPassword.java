ea8306a66d07b78b422e0d9ee9a3463b2edb304f
Dorian <dorianc.vieira@gmail.com>
2022-02-27 16:27:13 -0300

    Challenge two

--- src/capgemini/twoquestion/StrongPassword.java
+++ src/capgemini/twoquestion/StrongPassword.java
@@ -7,7 +7,7 @@ public class StrongPassword {
 	public static void main(String[] args) {
 
 		Scanner scan = new Scanner(System.in);
-		String password = scan.next();		
+		final String password = scan.next();		
 		final int size = 6;
 		if(password.length() < size) {
 			System.out.println(size - password.length());
