04e4b290e40c0fce403314d904e3fcd83ff4cfd5
Dorian <dorianc.vieira@gmail.com>
2022-02-27 14:48:30 -0300

    Challenge one

---
+++ src/capgemini/onequestion/Ladder.java
@@ -0,0 +1,30 @@
+package capgemini.onequestion;
+
+import java.util.Scanner;
+
+public class Ladder {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		System.out.printf("n = ");
+		int n = scan.nextInt();
+
+		for (int i = 1; i <= n; i++) {
+			int step = i;
+			int complement=n;
+			
+			while (step < n) {
+				System.out.printf(" ");
+				step++;
+			}
+			
+			while (complement+i > n) {
+				System.out.printf("*");
+				complement--;
+			}
+			System.out.println("");
+		}
+		scan.close();
+	}
+}
